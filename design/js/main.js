23ced8e1e11f76f10e634fc5cc1714dddfe0dd24
ahmed <ahmed.assv@gmail.com>
2019-03-20 15:54:05 +0100

    push theme

---
+++ design/js/main.js
@@ -0,0 +1,11 @@
+$('#myCarousel').carousel({
+           interval: 3000,
+ });
+
+$(function(){
+    'use strict';
+   var  win=$(window).height(),
+       upper=$('.upper_bar').innerHeight(),
+       nav=$('.navbar').innerHeight();
+     $('.slider').height(win-(upper+nav));
+});
